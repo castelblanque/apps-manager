27158b51049a72e3fc3ec73c9c8a1d41906df86b
Rafa Castelblanque <rafa@blanbo.com>
2021-08-03 23:15:45 +0200

    Added Helm chart installation

--- src/main/java/com/interview/kubeapps/controllers/ChartsController.java
+++ src/main/java/com/interview/kubeapps/controllers/ChartsController.java
@@ -6,9 +6,17 @@ import com.interview.kubeapps.generated.model.ChartInstallationRequestData;
 import com.interview.kubeapps.generated.model.ChartRequest;
 import com.interview.kubeapps.services.ChartsService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
 
@@ -39,6 +47,42 @@ public class ChartsController implements ChartsApi {
     public ResponseEntity<ChartInstallationRequestData> installNewChart(ChartRequest chartRequest) {
         final ChartInstallationRequestData outcome = chartService.install(chartRequest.getId(), chartRequest.getName());
         return new ResponseEntity<>(outcome, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/charts/new",
+            produces = { "application/json" },
+            consumes = { "application/json" },
+            method = RequestMethod.POST)
+    public ResponseEntity<StreamingResponseBody> installChart(@RequestBody ChartRequest chartRequest) throws InterruptedException, IOException {
+
+        Runtime r2 = Runtime.getRuntime();
+        Process p2 = r2.exec("helm repo add bitnami https://charts.bitnami.com/bitnami");
+        p2.waitFor();
+
+
+        StreamingResponseBody responseBody = outputStream -> {
+
+            try {
+                Runtime r = Runtime.getRuntime();
+                Process p = r.exec("helm install " + chartRequest.getName() + " " + chartRequest.getId());
+                p.waitFor();
+
+                int numberOfBytesToWrite;
+                byte[] data = new byte[1024];
+                while ((numberOfBytesToWrite = p.getInputStream().read(data, 0, data.length)) != -1) {
+                    System.out.println("Writing some bytes..");
+                    outputStream.write(data, 0, numberOfBytesToWrite);
+                }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        };
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(responseBody);
     }
 
 }
--- src/main/java/com/interview/kubeapps/services/ChartsService.java
+++ src/main/java/com/interview/kubeapps/services/ChartsService.java
@@ -3,11 +3,32 @@ package com.interview.kubeapps.services;
 import com.interview.kubeapps.generated.model.ChartInstallationRequestData;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.UUID;
+
 @Service
 public class ChartsService {
 
     public ChartInstallationRequestData install(String id, String name) {
-        return null;
+        final ChartInstallationRequestData outcome = new ChartInstallationRequestData();
+        outcome.setId(UUID.randomUUID());
+        return outcome;
+    }
+
+    private void runCommand(String command) throws IOException, InterruptedException {
+        Runtime r = Runtime.getRuntime();
+        Process p = r.exec(command);
+        p.waitFor();
+        BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line = "";
+
+        while ((line = b.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        b.close();
     }
 
 }
